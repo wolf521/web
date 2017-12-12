02d5473a253cb27e90ce87ce0bd56a55e777e7f6
cuizhixiang <15953185712@163.com>
2017-12-12 17:51:51 +0800

    监听

--- demo/src/main/java/model/Student.java
+++ demo/src/main/java/model/Student.java
@@ -13,29 +13,13 @@ public class Student implements HttpSessionActivationListener,Serializable{
 
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
     @Override
     public void sessionWillPassivate(HttpSessionEvent httpSessionEvent) {
-        System.out.print("钝化"+httpSessionEvent.getSource().toString());
+
     }
 
     @Override
     public void sessionDidActivate(HttpSessionEvent httpSessionEvent) {
-        System.out.print("活化"+httpSessionEvent.getSource().toString());
+
     }
 }
