b95a13bb9622d3c6520e448e6b3fec3542c7dc6c
cuizhixiang <15953185712@163.com>
2017-12-26 15:51:33 +0800

    ,

--- demo/src/main/java/web_service/web_user_service/UserService.java
+++ demo/src/main/java/web_service/web_user_service/UserService.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 /**
  * Created by Administrator on 2017/11/17.
+ * 用户service
  */
 public class UserService {
     private ServiceCenterUserService serviceCenterUserService = new main.java.service.UserService();
